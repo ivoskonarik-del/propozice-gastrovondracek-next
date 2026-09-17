532fbec67cc368f8d90eb54a39000ff6e6d51802
Weboviny Deploy <deploy@weboviny.cz>
2026-09-17 17:18:47 +0200

    Deploy 2026-09-17

---
+++ _next/static/chunks/app/jidelnicek/page-e885d754f60d1472.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[519],{7567:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.bind(n,4515)),Promise.resolve().then(n.bind(n,1301))}},e=>{var s=s=>e(e.s=s);e.O(0,[896,301,515,441,517,358],()=>s(7567)),_N_E=e.O()}]);
